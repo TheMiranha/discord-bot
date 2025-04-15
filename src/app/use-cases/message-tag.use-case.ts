3d77653a989ef7bb5b975a0808336fc02f7a412b
TheMiranha <lxbrmiranda@gmail.com>
2025-04-14 23:35:42 -0300

    feat: more chat emojis

--- src/app/use-cases/message-tag.use-case.ts
+++ src/app/use-cases/message-tag.use-case.ts
@@ -10,6 +10,22 @@ export class MessageTagUseCase {
     if (userId === '705176526546665604') {
       return '🦁'
     }
+
+    if (userId === '364951433570680852') {
+      return '🐰'
+    }
+
+    if (userId === '330575773645668365') {
+      return '🐸'
+    }
+
+    if (userId === '524950267536605185') {
+      return '🦄'
+    }
+
+    if (userId === '301545017661718528') {
+      return '🐮'
+    }
   }
 
 }
